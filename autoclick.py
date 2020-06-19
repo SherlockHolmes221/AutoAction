bdb9ba4e5120103e2094dbb963c78ed48767f066
QuXian <973162258@qq.com>
2020-06-19 13:07:43 +0800

    Update autoclick.py

--- autoclick.py
+++ autoclick.py
@@ -27,10 +27,10 @@ def scut():
         "//*[@id='pd']").send_keys(os.environ['SCUT_PASSWORD'])
     # 在输入用户名和密码之后,点击登陆按钮
     browser.find_element_by_xpath("//*[@id='index_login_btn']").click()
-    time.sleep(10)
+    time.sleep(50)
     try:
         browser.find_element_by_xpath("//*[@id='app']/div/div/div[2]/div[3]/button").click()
-        browser.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[3]/button[1]").click()
+        # browser.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[3]/button[1]").click()
 
         print("华工申报成功")
         time.sleep(3)
